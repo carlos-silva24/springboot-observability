8c8a630ceda70051609adfb411f3d64417b6e645
Carlos Silva <carlos.silva@kuehne-nagel.com>
2024-02-23 17:10:51 +0000

    official support for jersey with micrometer

--- src/main/java/com/github/carlossilva/springbootobservability/config/JerseyConfig.java
+++ src/main/java/com/github/carlossilva/springbootobservability/config/JerseyConfig.java
@@ -2,20 +2,28 @@ package com.github.carlossilva.springbootobservability.config;
 
 import org.glassfish.jersey.server.ResourceConfig;
 import org.glassfish.jersey.servlet.ServletProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
 
+import io.micrometer.observation.ObservationRegistry;
 import jakarta.ws.rs.ApplicationPath;
 
 /**
  * {@link Configuration} for REST (JAX-RS) services.
  */
-@Configuration
+@Component
 @ApplicationPath("/")
 public class JerseyConfig extends ResourceConfig {
 
     public JerseyConfig() {
         packages("com.github.carlossilva.springbootobservability.rest");
         property(ServletProperties.FILTER_FORWARD_ON_404, true);
+    }
+
+    @Bean
+    ObservationRegistry observationRegistry() {
+        return ObservationRegistry.create();
     }
 
 }
